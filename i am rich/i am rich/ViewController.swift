6f895f9025d22310fe191df5fb0016bb343068b3
Raghunandan <ragsofblr@gmail.com>
2023-02-19 12:22:29 +0530

    i am rich in progress

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Raghunandan P T on 19/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
