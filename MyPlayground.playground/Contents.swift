b173e3e616134c5c37c2bc0e0c04b66a949e9af2
Raghunandan <ragsofblr@gmail.com>
2023-03-10 12:10:11 +0530

    dice

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+let charsForPass = [ "a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
+
+let password1 = charsForPass.randomElement()! + charsForPass.randomElement()!
+
+
+print(password1)
